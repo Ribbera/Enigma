fd4fd426f0c36e7fb06696b6947ad6ce00affe4c
ribbera <ribberadev@gmail.com>
2025-12-14 14:17:46 +0100

    feat: agregar funcion descifrar

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
+import unicodedata
+from rotor import Rotor
 
 def normalizar_texto(texto):
-    # Quitar acentos y dejar solo letras
+    texto = unicodedata.normalize("NFD", texto)
     limpio = ""
-    for letra in texto:
+    for caracter in texto:
+        if unicodedata.category(caracter) == "Mn":
+            continue
+        letra = caracter.upper()
         if letra.isalpha():
-            limpio += letra.upper()
+            limpio += letra
     return limpio
 
+def cifrar_mensaje():
+    print("\nCIFRADO")
+
+    #pedimos las posciones inciales 
+    ventanas = input("3 letras iniciales (ej: ABC): ").upper()
+
+    #cargamos los rotores en sus variables correspondientes
+    rotor1 = Rotor.from_file("Rotor1.txt", ventanas[0])
+    rotor2 = Rotor.from_file("Rotor2.txt", ventanas[1])
+    rotor3 = Rotor.from_file("Rotor3.txt", ventanas[2])
+
+    mensaje = input("Mensaje: ")
+    mensaje_limpio = normalizar_texto(mensaje)
+
+    mensaje_cifrado = ""
+    for letra in mensaje_limpio:
+        if rotor1.avanzar():
+            if rotor2.avanzar():
+                rotor3.avanzar()
+
+        paso = rotor1.forward(letra)
+        paso = rotor2.forward(paso)
+        paso = rotor3.forward(paso)
+        mensaje_cifrado += paso
+
+    print("Cifrado:", mensaje_cifrado)
+
 def main():
-    print("Probando normalizacion de texto")
-    mensaje = input("Escribe algo: ")
-    resultado = normalizar_texto(mensaje)
-    print("Resultado:", resultado)
+    cifrar_mensaje()
 
 if __name__ == "__main__":
     main()
